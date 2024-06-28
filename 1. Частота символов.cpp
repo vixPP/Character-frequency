511b0c169bbe573a827aff2a8f8a4a6c895ce3c1
vix <churink9@gmail.com>
2024-06-28 18:34:09 +0300

    first commit

---
+++ 1. Частота символов.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+static bool sortValue(const std::pair<char, int>& a, const std::pair<char, int>& b)
+{
+    return a.second > b.second;
+}
+
+int main() 
+{
+    std::string input;
+    std::cout << "Введите текст: ";
+    std::getline(std::cin, input);
+
+    std::map<char, int> frequencies;
+
+    for (char c : input) 
+    {
+        if (std::isalpha(c)) 
+        {
+            frequencies[std::tolower(c)]++;
+        }
+    }
+
+    std::vector<std::pair<char, int>> sortedFrequencies(frequencies.begin(), frequencies.end());
+
+    std::sort(sortedFrequencies.begin(), sortedFrequencies.end(), sortValue);
+
+    for (const auto& pair : sortedFrequencies) 
+    {
+        std::cout << pair.first << ": " << pair.second << "\n";
+    }
+
+    return 0;
+}
